dbfbfef940ceaa23635d2cc798aeec81b89e5667
Jackson-Muendo <91467851+Jackson-Muendo@users.noreply.github.com>
2022-11-26 16:52:28 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,6 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome ALL!" />
-        <p className="description">
-         HERE COMES
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
